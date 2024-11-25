54a74975c7d3287220f214c42b04207f72e49041
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-25 20:16:43 +0800

    Create xfce.sh

---
+++ files/xfce.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+###
+wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/files/bash.bashrc
+###
+apt install -y kali-linux-core xfce4 xfce4-goodies xfce4-terminal kali-desktop-xfce xfce4-whiskermenu-plugin dbus-x11 kali-defaults kali-themes kali-menu firefox-esr tigervnc-standalone-server kali-linux-default
