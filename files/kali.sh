94d3cd6cf5cc2b77d59983a5855c673f1d974c0b
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-26 23:56:51 +0800

    Update kali.sh

--- files/kali.sh
+++ files/kali.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="kali"
 TARBALL_URL['aarch64']="https://github.com/xiv3r/proot-distro-kali/releases/download/proot-distro/kali-rolling_arm64-rootfs.tar.xz"
-TARBALL_SHA256['aarch64']="c49fab64df4b427d240c0af078cca4a58ed1db6c66a321d5f3ab479007a64b1f"
+TARBALL_SHA256['aarch64']="408dc56550c1df9e36b5fc98aebb45528d4b377deb097a291f7b65bc5a624f7e"
 distro_setup() {
         # Configure en_US.UTF-8 locale.
         sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
