dc6ff3cefbb1868032251b39c81ab55549ccbe17
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-27 12:59:32 +0800

    Create zsh.sh

---
+++ zsh/zsh.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -O /etc/zsh/auto.zsh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/zsh/auto.zsh
+wget -O /etc/zsh/syntax.zsh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/zsh/syntax.zsh
