d5d63099a832d1e087d1b3c5c92f2c8414d187b4
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-27 13:56:53 +0800

    Update zsh.sh

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-wget -O /usr/share/zsh/auto.zsh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/zsh/auto.zsh
-###
-wget -O /usr/share/zsh/syntax.zsh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/zsh/syntax.zsh
 
+apt install unzip -y
+###
+wget https://github.com/xiv3r/proot-distro-kali/blob/main/zsh/.zsh.zip
+###
+unzip -d .zsh.zip
+###
+echo "
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+" >> ~/.zshrc
