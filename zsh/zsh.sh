b7f23c705f7e0756945f9af339eaa8573eb11350
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-27 14:53:59 +0800

    Update zsh.sh

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 ###
-wget -O ~/.zshrc https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/files/.zshrc
+wget -O .zshrc https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/files/.zshrc
 ###
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && source ~/.zshrc
-###
+git clone https://github.com/zsh-users/zsh-autosuggestions .zsh/zsh-autosuggestions
