1b73a2465deefa8ee8688d82966fa67d1fc03680
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-27 14:05:12 +0800

    Update zsh.sh

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -3,10 +3,12 @@
 
 apt install unzip -y
 ###
-wget https://github.com/xiv3r/proot-distro-kali/blob/main/zsh/.zsh.zip
+wget https://github.com/xiv3r/proot-distro-kali/blob/main/zsh/zsh.zip
 ###
-unzip -d .zsh.zip
+unzip -d zsh.zip
+###
+mv -r zsh .zsh
 ###
 echo "
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-" >> ~/.zshrc
+" >> ~/.zshrc && source ~/.zshrc
