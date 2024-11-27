86b1c0b6a6cc37590069e3726e3ec18148e284ec
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-27 13:01:35 +0800

    Update zsh.sh

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 wget -O /etc/zsh/auto.zsh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/zsh/auto.zsh
+###
 wget -O /etc/zsh/syntax.zsh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/zsh/syntax.zsh
+###
+source ~/.zshrc
