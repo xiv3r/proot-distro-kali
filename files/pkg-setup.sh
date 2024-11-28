84357af554f5ec2527cc0eaf482c4f9659431bc7
pipX-ux-wizard <pip-x-wizard.ch@pm.me>
2024-11-28 14:23:14 -0700

    Update and rename setup.sh to pkg-setup.sh
    #!/bin/sh
    chsh -s ["/~"]=$
    cat /data/data/com.termux/files/usr/etc/bash.bashrc
    if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
            command_not_found_handle() {
                    /data/data/com.termux/files/usr/libexec/termux/command-not-found "~$1"
    
                    ["/~]$ pkg upgradeable
    chmod a+x $PREFIX
    [~]$ pkg upgradeable
    chmod a+x $PREFIX
    apt install -y proot-distro 
    ###
    wget -O $PREFIX/etc/proot-distro/kali.sh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/files/kali.sh
    ###
    chmod a+x $PREFIX/etc/proot-distro/kali.sh
    ###
    proot-distro install kali

--- files/pkg-setup.sh
+++ files/pkg-setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-cat <<- CONF > /data/data/com.termux/files/usr/etc/bash.bashrc
+chsh -s ["/~"]=$
+cat /data/data/com.termux/files/usr/etc/bash.bashrc
 if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
         command_not_found_handle() {
                 /data/data/com.termux/files/usr/libexec/termux/command-not-found "~$1"
 
-                [~]$ pkg upgradeable
+                ["/~]$ pkg upgradeable
 chmod a+x $PREFIX
 [~]$ pkg upgradeable
 chmod a+x $PREFIX
