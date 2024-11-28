3c3df2424be2df63dd9b0bdc665a90ffdf88cf16
pipX-ux-wizard <pip-x-wizard.ch@pm.me>
2024-11-28 14:16:12 -0700

    Update setup.sh
    #!/bin/sh
    cat <<- CONF > /data/data/com.termux/files/usr/etc/bash.bashrc
    if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
            command_not_found_handle() {
                    /data/data/com.termux/files/usr/libexec/termux/command-not-found "~$1"
    
                    [~]$ pkg upgradeable
    chmod a+x $PREFIX
    [~]$ pkg upgradeable
    chmod a+x $PREFIX
    apt install -y proot-distro 
    ###
    wget -O $PREFIX/etc/proot-distro/kali.sh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/files/kali.sh
    ###
    chmod a+x $PREFIX/etc/proot-distro/kali.sh
    ###
    proot-distro install kali

--- files/setup.sh
+++ files/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+cat <<- CONF > /data/data/com.termux/files/usr/etc/bash.bashrc
+if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+        command_not_found_handle() {
+                /data/data/com.termux/files/usr/libexec/termux/command-not-found "~$1"
 
+                [~]$ pkg upgradeable
+chmod a+x $PREFIX
+[~]$ pkg upgradeable
+chmod a+x $PREFIX
 apt install -y proot-distro 
 ###
 wget -O $PREFIX/etc/proot-distro/kali.sh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/files/kali.sh
