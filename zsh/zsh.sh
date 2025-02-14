55618ddb02fd72a259dc884864c0bce65b30adb4
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-02-14 17:12:02 +0800

    Delete zsh directory

--- zsh/zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-###
-wget -O .zshrc https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/files/.zshrc
-###
-git clone https://github.com/zsh-users/zsh-autosuggestions .zsh/zsh-autosuggestions
