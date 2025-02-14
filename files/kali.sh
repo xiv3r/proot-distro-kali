31a1eea310d007bc19fd4e780a87762d44761a99
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-02-14 17:11:50 +0800

    Delete files directory

--- files/kali.sh
+++
@@ -1,8 +0,0 @@
-DISTRO_NAME="kali"
-TARBALL_URL['aarch64']="https://github.com/xiv3r/proot-distro-kali/releases/download/proot-distro/kali-arm64.tar.xz"
-TARBALL_SHA256['aarch64']="408dc56550c1df9e36b5fc98aebb45528d4b377deb097a291f7b65bc5a624f7e"
-distro_setup() {
-        # Configure en_US.UTF-8 locale.
-        sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
-        run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
-}
--- files/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-apt install -y proot-distro 
-###
-wget -O $PREFIX/etc/proot-distro/kali.sh https://raw.githubusercontent.com/xiv3r/proot-distro-kali/refs/heads/main/files/kali.sh
-###
-chmod a+x $PREFIX/etc/proot-distro/kali.sh
-###
-proot-distro install kali
